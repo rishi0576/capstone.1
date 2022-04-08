7e2a21b4d3984ec46b49ba4eb688e578ec734109
rishi0576 <101387909+rishi0576@users.noreply.github.com>
2022-04-08 11:58:59 +0530

    Add files via upload

---
+++ ghost0_5.py
@@ -0,0 +1,108 @@
+import pygame, sys,random
+
+pygame.init()
+pygame.mixer.init()
+
+clock=pygame.time.Clock()
+width=600
+height=600
+screen = pygame.display.set_mode((width,height))
+  
+#load the images in dict
+images={}
+images["bg"] = pygame.image.load("tower.png").convert_alpha()
+images["ghost"] = pygame.image.load("ghost-standing.png").convert_alpha()
+images["window"] = pygame.image.load("door.png").convert_alpha()
+groundy=-50
+
+score=0
+score_font=pygame.font.Font('freesansbold.ttf', 25)
+
+class Ghost:
+    speed=10
+    g=1.0 
+    rect= pygame.Rect(300,250,100,100)
+
+    def gravity(self):
+        self.speed=self.speed+self.g
+        self.rect.y= self.rect.y + self.speed
+
+            
+    def moveLeft(self):
+        self.rect.x-=20
+    def moveRight(self):
+        self.rect.x+=20
+    def jump(self):
+        self.speed=-10
+    def display(self): 
+        pygame.draw.rect(screen,(150,100,50),self.rect)
+class Window:
+   
+    def __init__(self,y): 
+        self.speed=3
+        self.gap=random.randint(100, 400)
+        self.rect1=pygame.Rect(self.gap,y+250 ,40,20)
+        self.rect2=pygame.Rect(self.gap,y+140,50,5)
+        self.rect3=pygame.Rect(self.gap,y+100,50,10)
+    def display(self):  
+        screen.blit(images["window"],self.rect1)
+    
+    
+    def move(self):
+        self.rect1.y+=self.speed
+        self.rect2.y+=self.speed
+        if(self.rect1.y>600):
+            self.rect1.y =-200
+            self.rect2.y=-30
+            self.rect1.x=random.randint(200, 500)
+            self.rect2.x=self.rect1.x
+
+          
+
+state="play"
+ghost= Ghost()
+
+w1=Window(-200) 
+w2=Window(-500)     
+while True:    
+    screen.fill((50,150,255))
+    screen.blit(images["bg"],[0,groundy])
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            sys.exit() 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                ghost.moveLeft()
+            
+            if event.key == pygame.K_RIGHT:
+               ghost.moveRight()
+            if event.key == pygame.K_SPACE:
+               ghost.jump()
+            
+               
+   
+    w1.move()
+    w2.move()
+    w1.display()
+    w2.display()
+        
+    
+    ghost.gravity()
+    ghost.display()
+    
+    if state=="over":
+        over_text=score_font.render("Game Over", False, (255,255,0))  
+        screen.blit(over_text,[230,250]) 
+    
+
+    
+
+    pygame.display.update() 
+    clock.tick(30) 
+    
+    
+    
+    
+
+ 
